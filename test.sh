457e50432f0a3d5b0bafb57c43eadf375377d2a5
tohru <jhtohru@gmail.com>
2022-06-07 14:38:59 -0300

    test.sh build execs in a local folder

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 testsdir="./tests"
-execfile="/tmp/${1}_test"
+bindir="${testsdir}/bin"
+execfile="${bindir}/${1}_test"
 testfile="${testsdir}/${1}_test.c"
 auxfiles="${testsdir}/test_utils.c"
 libfile="./libft.a"
+
+mkdir -p "$bindir"
 
 gcc -Wall -Wextra -Werror -o "$execfile" \
 	-I "$testsdir" \
