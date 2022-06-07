055be63dd0a12fc50ca0f7b60e8e351449fa3152
tohru <jhtohru@gmail.com>
2022-06-07 11:49:29 -0300

    Fix shell script to make testing easier

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-testsdir="./test"
-execfile="${testsdir}/${1}__test"
-testfile="${execfile}.c"
-auxfiles="${testsdir}/print_test_title.c ${testsdir}/print_test_result.c"
+testsdir="./tests"
+execfile="/tmp/${1}_test"
+testfile="${testsdir}/${1}_test.c"
+auxfiles="${testsdir}/test_utils.c"
+libfile="./libft.a"
 
 gcc -Wall -Wextra -Werror -o "$execfile" \
-	"$testfile" ./*.c "$testsdir"/print_test_title.c "$testsdir"/print_test_result.c \
+	-I "$testsdir" -I ./ \
+	"$testfile" "$auxfiles" "$libfile" \
 	&& "$execfile"
