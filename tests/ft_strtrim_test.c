9bc1e19c806bfda7dd01d305585211ef73ca4029
jlourencobdev <joao.lourenco@bairesdev.com>
2022-06-19 15:16:14 -0300

    Improve ft_strtrim tests

--- tests/ft_strtrim_test.c
+++ tests/ft_strtrim_test.c
@@ -6,7 +6,7 @@
 /*   By: jmenezes <jmenezes@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 09:50:52 by jmenezes          #+#    #+#             */
-/*   Updated: 2022/06/14 07:25:48 by jmenezes         ###   ########.fr       */
+/*   Updated: 2022/06/19 15:15:14 by jmenezes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,52 +43,63 @@
 // ft_strtrim must allocate memory and returns a copy of s without the
 // characters specified in set removed from the beginning and the end of the
 // string.
-int test_ft_strtrim_success(void)
+int	test_ft_strtrim_success(void)
 {
-    char    str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
-    char    *tstr;
-    int     res;
+	char	str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
+	char	*tstr;
+	int		res;
 
-    tstr = ft_strtrim(str, ".:|");
-    res = strcmp(tstr, "a.b:|:c.:|:.d:|:e") == 0;
-    free(tstr);
-    return (res);
+	tstr = ft_strtrim(str, ".:|");
+	res = strcmp(tstr, "a.b:|:c.:|:.d:|:e") == 0;
+	free(tstr);
+	return (res);
 }
 
 // ft_strtrim must take no effect over strings which its extremities have no
 // character specified in set.
-int test_ft_strtrim_no_effect(void)
+int	test_ft_strtrim_no_effect(void)
 {
-    char    str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
-    char    *tstr;
-    int     res;
+	char	str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
+	char	*tstr;
+	int		res;
 
-    tstr = ft_strtrim(str, "abc");
-    res = strcmp(tstr, str) == 0;
-    free(tstr);
-    return (res);
+	tstr = ft_strtrim(str, "abc");
+	res = strcmp(tstr, str) == 0;
+	free(tstr);
+	return (res);
 }
 
 // ft_strtrim must take no effect if set is an empty string.
-int test_ft_strtrim_empty_set(void)
+int	test_ft_strtrim_empty_set(void)
 {
-    const char  str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
-    char        *tstr;
-    int         res;
+	const char	str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
+	char		*tstr;
+	int			res;
 
-    tstr = ft_strtrim(str, "");
-    res = strcmp(tstr, str) == 0;
-    free(tstr);
-    return (res);
+	tstr = ft_strtrim(str, "");
+	res = strcmp(tstr, str) == 0;
+	free(tstr);
+	return (res);
 }
 
-// ft_strtrim must return NULL if the allocation fails.
-// todo
+// ft_strtrim must return NULL if memory allocation fails.
+int	test_ft_strtrim_nomem(void)
+{
+	char	str[] = ".:|||:.a.b:|:c.:|:.d:|:e.:|||:.";
+	char	*tstr;
+
+	simulate_malloc_failure();
+	tstr = ft_strtrim(str, ".:|");
+	free(tstr);
+    reset_malloc();
+	return (tstr == NULL);
+}
 
 int main(void)
 {
 	print_test_result("test_ft_strtrim_success", test_ft_strtrim_success());
 	print_test_result("test_ft_strtrim_no_effect", test_ft_strtrim_no_effect());
 	print_test_result("test_ft_strtrim_empty_set", test_ft_strtrim_empty_set());
-    return (0);
+	print_test_result("test_ft_strtrim_nomem", test_ft_strtrim_nomem());
+	return (0);
 }
